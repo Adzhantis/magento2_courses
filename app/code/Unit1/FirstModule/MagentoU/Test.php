2fc2b609d5e2e0ae248251b1e5d3086f2ab2cd0d
adzhantis <adzhantis@ukr.net>
2017-12-07 22:57:58 +0200

    files for module 67

---
+++ app/code/Unit1/FirstModule/MagentoU/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ally
+ * Date: 07.12.2017
+ * Time: 22:48
+ */
+class Test
+{
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Catalog\Model\ProductFactory $productFactory,
+        \Magento\Checkout\Model\Session $session,
+        \Unit1\FirstModule\Api\ProductRepositoryInterface $unit1ProductRepository,
+        $justAParameter = false,
+        array $data
+    ) {
+    }
+}
