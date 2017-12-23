eef129f93d9de5772d4e71316aa9eb0e9703a8a8
adzhantis <adzhantis@ukr.net>
2017-12-23 16:26:25 +0200

    module 3 router works

--- app/code/Unit1/SecondModule/Controller/Router.php
+++ app/code/Unit1/SecondModule/Controller/Router.php
@@ -4,17 +4,22 @@ namespace Unit1\SecondModule\Controller;
 
 class Router implements \Magento\Framework\App\RouterInterface
 {
+
+    public  $actionFactory;
+
     public function __construct(\Magento\Framework\App\ActionFactory $actionFactory) {
         $this->actionFactory = $actionFactory;
     }
     public function match(\Magento\Framework\App\RequestInterface $request) {
         $info = $request->getPathInfo();
-        echo '<pre>'; var_dump($info);die;
+
         if (preg_match("%^/(test)-(.*?)-(.*?)$%", $info, $m)) {
+            //echo '<pre>'; var_dump($info, 1);die;
             $request->setPathInfo(sprintf("/%s/%s/%s", $m[1], $m[2], $m[3]));
             return $this->actionFactory->create('Magento\Framework\App\Action\Forward',
                 ['request' => $request]);
         }
+        //echo '<pre>'; var_dump($info, 2);die;
         return null;
     }
 }
