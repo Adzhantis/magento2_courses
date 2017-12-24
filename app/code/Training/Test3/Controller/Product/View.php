9f226f4626cd962bd264a7fa20bbb1457898a623
adzhantis <adzhantis@ukr.net>
2017-12-24 10:32:39 +0200

    module 3 controller view for product works

--- app/code/Training/Test3/Controller/Product/View.php
+++ app/code/Training/Test3/Controller/Product/View.php
@@ -7,18 +7,19 @@ class View extends \Magento\Framework\App\Action\Action
 
     public function execute()
     {
-        echo "ONE"; exit;
+     //  echo "ONE"; exit;
     }
 
     public function beforeExecute()
     {
-        //echo "BEFORE<BR>"; exit;
+       // echo "BEFORE<BR>"; exit;
     }
 
     public function afterExecute(
         \Magento\Catalog\Controller\Product\View $controller, $result)
-    {
-        //echo "AFTER"; exit;
+    {//echo '<pre>'; var_dump(get_class($result));die;
+        // echo "AFTER"; exit;
+        return $result;
     }
 
 }
